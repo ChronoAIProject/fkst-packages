09b8712a1232a151d3881ee04a061ccddcb1f19a
Loning <aloning@gmail.com>
2026-06-19 05:33:25 +0800

    auto-fix #1108: saga handler allowlist migration slice: 12b0873f0a5f5bcf

--- packages/github-proxy/tests/std_saga_test.lua
+++ packages/github-proxy/tests/std_saga_test.lua
@@ -35,10 +35,11 @@ return {
     t.eq(acted, 1)
   end,
 
-  test_department_preserves_graph_json_spec_flag = function()
+  test_department_preserves_observability_spec_metadata = function()
     local module = saga.department({
       consumes = { "demo" },
       graph_json = true,
+      stall_window = "2m",
     }, {
       done = function(_event)
         return true
@@ -48,6 +49,7 @@ return {
     })
 
     t.eq(module.spec.graph_json, true)
+    t.eq(module.spec.stall_window, "2m")
   end,
 
   test_department_skips_when_done = function()
